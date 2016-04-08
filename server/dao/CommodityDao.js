483832f0cde804f8d3547ce10a5bfb467e3bc13c
Yao Feng <yao.feng@sap.com>
2016-04-08 17:56:37 +0800

    1
    Change-Id: I12900bd2d9f14b6d0b9c4cc77d10f682e48ed740

---
+++ server/dao/CommodityDao.js
@@ -0,0 +1,17 @@
+module.exports = {
+	add: function (Commodity) {
+		 return true;
+	},
+
+	delete: function (id) {
+		  return true;
+	},
+
+	update: function (Commodity) {
+		 return true; 
+	},
+
+	list: function (pageVo) {
+		  return "{a:1}";
+	}
+}
